c5380ced4968bfef511751e9622f60a05782c175
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2021-03-24 09:41:01 -0300

    closes Botão de voltar ao topo #12

--- src/js/app.js
+++ src/js/app.js
@@ -87,3 +87,18 @@ $(".btn-clear").on('click', function() {
     $(".custom-checkbox input:checkbox").prop('checked', false);
     $(".custom-checkbox input:checkbox").closest("label").removeClass("active");
 })
+
+var mybutton = document.getElementById("totop");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
